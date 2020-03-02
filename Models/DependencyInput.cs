1d0faefcb3a7f19715564254019128a450c88a9f
rajaprabhu14 <61670674+rajaprabhu14@users.noreply.github.com>
2020-03-02 01:42:08 +0000

    Add files via upload

---
+++ Models/DependencyInput.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dependency_Solution.Models
+{
+    public class Dependency
+    {
+        public string Input { get; set; }
+    }
+
+    public class DependencyInput
+    {
+        public List<Dependency> Inputs { get; set; }
+    }
+}
